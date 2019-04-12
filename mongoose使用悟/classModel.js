ea7d31621d7643562bde6b6081569dc2c1479094
Zgo <zguoby@gmail.com>
2019-04-12 16:50:08 +0800

    Update classModel.js

--- mongoose使用悟/classModel.js
+++ mongoose使用悟/classModel.js
@@ -1,31 +1,47 @@
+const db = require('./connect');
+const url = require('url');
+
+const _connect = function(name,_func){
+    global._client.connect(function(err) {
+        let u = url.parse(_client.s.url),search = new URLSearchParams(u.search),
+        authSource = search.get('authSource');
+        if(err) return _func(err);
+        else {
+            return _func(null,global._client.db(authSource).collection(name))
+        }
+    });
+}
+
 class parentModel{
     constructor(props){
         this.name = props;
     }
     find(queryJson,options,func){
-        let name = this.name;
-        if(func){
-            func = func;
-        }else{
+        if(typeof options == 'function'){
             func = options;
-            options = null;
+            options = {};
         }
-        global._client.connect(function(err) {
+        let name = this.name,
+        _funca = (err,docs)=>{
+            if(err) func(err);
+            else func(err,docs);
+        }
+        _connect(name,function(err,collection) {
             if(err){
                 func(err);
             }else{
-                if(options) global._client.db('nodercms').collection(name).find(queryJson,options).toArray(func);
-                else global._client.db('nodercms').collection(name).find(queryJson).toArray(func)
+                if(options) collection.find(queryJson,options).toArray(_funca);
+                else collection.find(queryJson).toArray(_funca)
             }
-            global._client.close();
         });
     } 
 }
 
 class Model extends parentModel{
-    constructor(props){
-        super(props);
-    }
 }
-
+var m = new Model('navs');
+m.find({},function(err,docs){
+    if(err) console.log(err);
+    else console.log(docs[0]._id);
+})
 module.exports = Model;
