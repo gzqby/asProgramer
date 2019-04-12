7032cd1fb09bac190315f37c2c6895643f79ceaa
Zgo <zguoby@gmail.com>
2019-04-12 15:12:14 +0800

    Update classModel.js

--- mongoose使用悟/classModel.js
+++ mongoose使用悟/classModel.js
@@ -2,13 +2,20 @@ class parentModel{
     constructor(props){
         this.name = props;
     }
-    find(queryJson,func){
+    find(queryJson,options,func){
         let name = this.name;
+        if(func){
+            func = func;
+        }else{
+            func = options;
+            options = null;
+        }
         global._client.connect(function(err) {
             if(err){
                 func(err);
             }else{
-                global._client.db('nodercms').collection(name).find(queryJson).toArray(func)
+                if(options) global._client.db('nodercms').collection(name).find(queryJson,options).toArray(func);
+                else global._client.db('nodercms').collection(name).find(queryJson).toArray(func)
             }
             global._client.close();
         });
